23d53244c21c1742f53ebc97216cd39814ef6a01
Ilias Andreou <iandreou@deloitte.gr>
2026-03-17 09:48:13 +0000

    batman

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+QUnit.config.autostart = false;
+
+sap.ui.require(["sap/com/bedigix/approvectrfe/test/integration/AllJourneys"
+], function () {
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"sap/com/bedigix/approvectrfe/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
